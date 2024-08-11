e46e7baccf82fd7c8de39f550a59a18345713dce
aungkanphyoo <aungkanphyo1920095@gmail.com>
2024-08-11 12:58:09 +0630

    first commit

---
+++ LabTest/no1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace LabTest
+{
+    partial class no1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.rnoTextBox = new System.Windows.Forms.TextBox();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.addressBox = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(174, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Roll number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(174, 158);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Name";
+            // 
+            // rnoTextBox
+            // 
+            this.rnoTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rnoTextBox.Location = new System.Drawing.Point(288, 66);
+            this.rnoTextBox.Name = "rnoTextBox";
+            this.rnoTextBox.ReadOnly = true;
+            this.rnoTextBox.Size = new System.Drawing.Size(225, 28);
+            this.rnoTextBox.TabIndex = 2;
+            // 
+            // nameBox
+            // 
+            this.nameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameBox.Location = new System.Drawing.Point(288, 152);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(221, 28);
+            this.nameBox.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(174, 236);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Address";
+            // 
+            // addressBox
+            // 
+            this.addressBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addressBox.Location = new System.Drawing.Point(288, 234);
+            this.addressBox.Name = "addressBox";
+            this.addressBox.Size = new System.Drawing.Size(221, 28);
+            this.addressBox.TabIndex = 5;
+            // 
+            // submitButton
+            // 
+            this.submitButton.BackColor = System.Drawing.Color.Blue;
+            this.submitButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.submitButton.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.submitButton.Location = new System.Drawing.Point(295, 322);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(186, 46);
+            this.submitButton.TabIndex = 7;
+            this.submitButton.Text = "Submit";
+            this.submitButton.UseVisualStyleBackColor = false;
+            this.submitButton.Click += new System.EventHandler(this.SubmitButton);
+            // 
+            // no1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.addressBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.rnoTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "no1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.no1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox rnoTextBox;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox addressBox;
+        private System.Windows.Forms.Button submitButton;
+    }
+}
+
---
+++ LabTest/no1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LabTest
+{
+    public partial class no1 : Form
+    {
+        public no1()
+        {
+            InitializeComponent();
+        }
+
+        private void no1_Load(object sender, EventArgs e)
+        {
+            string connection = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Aung Kan Phyo\C#\LabTest\labTestDatabase.mdf;Integrated Security=True;Connect Timeout=30;";
+            int rno = GetNextRno(connection);
+            rnoTextBox.Text = rno.ToString();
+        }
+
+        private int GetNextRno(string connection)
+        {
+            int lastRno = 0;
+            using (SqlConnection conn = new SqlConnection(connection))
+            {
+                string query = "SELECT MAX(rno) FROM Student";
+                SqlCommand command = new SqlCommand(query, conn);
+                conn.Open();
+                lastRno = (int)command.ExecuteScalar();
+            }
+                return lastRno+1;
+        }
+
+        private void SubmitButton(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\Aung Kan Phyo\C#\LabTest\labTestDatabase.mdf;Integrated Security=True;Connect Timeout=30;");
+            conn.Open();
+            string query = "INSERT INTO Student (rno,name,address) VALUES (@rno,@name,@address)";
+            SqlCommand command = new SqlCommand(query,conn);
+            command.Parameters.AddWithValue("@rno", int.Parse(rnoTextBox.Text));
+            command.Parameters.AddWithValue("@name", nameBox.Text);
+            command.Parameters.AddWithValue("@address", addressBox.Text);
+            command.ExecuteNonQuery();
+            conn.Close();
+            MessageBox.Show("Registration successful");
+        }
+    }
+}
